d846dbd42f414bc334690a9b739dee8ff53d4be1
Luis Mondesi <lemsx1@gmail.com>
2014-10-18 01:43:01 -0400

    adds missing method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :ensure_signup_complete,
     only: [:new, :create, :update, :destroy]
+  def ensure_signup_complete
+    return if action_name == 'finish_signup' # avoid infinite loop
+    if current_user && !current_user.email_verified?
+      redirect_to finish_signup_path(current_user)
+    end
+  end
 end
