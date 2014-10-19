1ac795025e2e1d3ae5fae60f6b833fc001e24a2d
Luis Mondesi <lemsx1@gmail.com>
2014-10-18 23:50:00 -0400

    fixes route problem

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, path_names: {
     sign_in: 'login',
     sign_out: 'logout',
+  }, controllers: {
     omniauth_callbacks: 'omniauth_callbacks'
-  }
+    }
 
   get 'dashboard' => 'dashboard#index'
 
