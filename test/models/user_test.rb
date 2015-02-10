8ec5d945c8ae6e0ab9fb6dc01586f3d076d31aca
Luis Mondesi <lemsx1@gmail.com>
2015-02-10 01:03:07 -0500

    ready for tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,10 +1,10 @@
-require 'test_helper'
-
-describe User do
-  before do
-    sign_in :user
-  end
-  it 'tests the truth' do
-    assert true
-  end
-end
+# require 'test_helper'
+# 
+# describe User do
+#   before do
+#     sign_in :user
+#   end
+#   it 'tests the truth' do
+#     assert true
+#   end
+# end
