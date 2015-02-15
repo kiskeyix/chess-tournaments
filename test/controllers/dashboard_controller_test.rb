09fe43829bb1db11c4f2af8d6d0a8c6e24d8dde4
Luis Mondesi <lemsx1@gmail.com>
2015-02-15 11:01:05 -0500

    simple test for users

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionController::TestCase
+  before do 
+    OmniAuth.config.test_mode = true
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter] 
+  end
   it "should get index" do
     #sign_in users(:user_one)
     get :index
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  it 'tests the truth' do
-    assert true
+  it 'tests user is inactive' do
+    users(:user_two).active.must_equal false
   end
 end
