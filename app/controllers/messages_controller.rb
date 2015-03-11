faef517633a3c3839c5c5dc02946afd5a44e360c
Luis Mondesi <lemsx1@gmail.com>
2015-03-10 22:14:48 -0400

    protect messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,5 +5,9 @@ class MessagesController < ApplicationController
   end
   def show
     @message = Message.find params[:id]
+    unless @message.user == current_user or @message.users.include? current_user
+      flash[:alert] = "You cannot see this message since it's not addressed to you or written by you!"
+      redirect_to dashboard_path
+    end
   end
 end
