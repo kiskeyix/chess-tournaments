e2619dcc476aad2ca5a3a96be628525ba757d514
Luis Mondesi <lemsx1@gmail.com>
2015-06-27 08:40:28 -0400

    comment out. only uncomment when absolutely needed

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,10 +23,14 @@ set :scm, :git
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml',
+                                                 'config/secrets.yml',
+                                                 'db/development.sqlite3')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids',
+                                               'tmp/cache', 'tmp/sockets',
+                                               'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 set :default_env, { path: "/opt/ruby/2.2/bin:$PATH" }
@@ -39,9 +43,10 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute :rake, 'tmp:create'
+        execute :rake, 'cache:clear'
+      end
     end
   end
 
