fa805e03e2135d1c2e2cb535441e0c4c6eeb1a9e
Luis Mondesi <lemsx1@gmail.com>
2015-06-28 20:12:26 -0400

    adds icon

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303060400) do
+ActiveRecord::Schema.define(version: 20150629000859) do
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
@@ -55,22 +55,22 @@ ActiveRecord::Schema.define(version: 20150303060400) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",               default: "",   null: false
+    t.string   "username",               default: "",    null: false
     t.boolean  "active",                 default: true
-    t.string   "firstname",              default: "",   null: false
-    t.string   "middlename",             default: "",   null: false
-    t.string   "lastname",               default: "",   null: false
-    t.string   "surname",                default: "",   null: false
-    t.string   "nickname",               default: "",   null: false
-    t.string   "birthday",               default: "",   null: false
-    t.string   "gender",                 default: "",   null: false
-    t.string   "image",                  default: "",   null: false
-    t.string   "email",                  default: "",   null: false
-    t.string   "encrypted_password",     default: "",   null: false
+    t.string   "firstname",              default: "",    null: false
+    t.string   "middlename",             default: "",    null: false
+    t.string   "lastname",               default: "",    null: false
+    t.string   "surname",                default: "",    null: false
+    t.string   "nickname",               default: "",    null: false
+    t.string   "birthday",               default: "",    null: false
+    t.string   "gender",                 default: "",    null: false
+    t.string   "image",                  default: "",    null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,    null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -79,11 +79,12 @@ ActiveRecord::Schema.define(version: 20150303060400) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,    null: false
+    t.integer  "failed_attempts",        default: 0,     null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["active"], name: "index_users_on_active"
