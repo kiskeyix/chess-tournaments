6a96366444d0e77102a6f49c55b8e080d06e071b
Luis Mondesi <lemsx1@gmail.com>
2015-09-30 08:26:27 -0400

    using sqlite for now

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,9 @@ set :scm, :git
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/database.yml',
                                                  'config/secrets.yml',
-                                                 'db/development.sqlite3')
+                                                 'db/development.sqlite3',
+                                                 'db/production.sqlite3',
+                                                )
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids',
