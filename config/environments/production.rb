2ab54356bbd604bb2e521f1e9e79f79bbe62b7eb
Luis Mondesi <lemsx1@gmail.com>
2015-09-30 08:14:02 -0400

    actually use protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = {
     host: 'chess.kiskeyix.org',
-    port: 443
+    protocol: 'https'
   }
   config.action_mailer.delivery_method = :sendmail
 end
