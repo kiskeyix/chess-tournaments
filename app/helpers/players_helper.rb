607643ab4b04ba43412abfa256aaf8211f351f25
Luis Mondesi <lemsx1@gmail.com>
2015-09-30 16:38:58 -0400

    gravatar id from Gravstastic

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,7 +1,9 @@
 module PlayersHelper
   def gravatar_image_url(user,format='pgn')
     # TODO add url encoded default image
-    hashsum = Digest::MD5.hexdigest user.email
-    "https://gravatar.com/avatar/#{hashsum}.#{format}"
+    "https://gravatar.com/avatar/#{gravatar_id(user.email)}.#{format}"
+  end
+  def gravatar_id(email)
+    Digest::MD5.hexdigest(email.to_s.downcase)
   end
 end
