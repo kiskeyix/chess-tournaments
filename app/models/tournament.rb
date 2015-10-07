3b5efe802dfd47f4c4d9ce550061c856d56587ed
Luis Mondesi <lemsx1@gmail.com>
2015-10-07 04:24:28 -0400

    displays division and tournament

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -8,4 +8,10 @@ class Tournament < ActiveRecord::Base
   def players
     Player.joins(:teams => :divisions).where('tournament_id = ?', id)
   end
+
+  # returns division for this tournament and player
+  # note that a user can only be competing with 1 team in 1 division of every tournament
+  def player_divisions(player)
+    Division.joins(:teams => :players).where('tournament_id = ? AND players.id = ?', id, player.id).first
+  end
 end
