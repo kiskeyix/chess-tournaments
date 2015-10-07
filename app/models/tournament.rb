1921ddae9bd7d93e16f215f7883d09ff3e6ec990
Luis Mondesi <lemsx1@gmail.com>
2015-10-07 05:01:02 -0400

    ready to start with lineups

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -12,6 +12,12 @@ class Tournament < ActiveRecord::Base
   # returns division for this tournament and player
   # note that a user can only be competing with 1 team in 1 division of every tournament
   def player_divisions(player)
-    Division.joins(:teams => :players).where('tournament_id = ? AND players.id = ?', id, player.id).first
+    Division.joins(:teams => :players).where('tournament_id = ? AND players.id = ?', id, player.id).uniq.last
+  end
+
+  # returns team for this tournament and player
+  # note that a user can only be competing with 1 team in 1 division of every tournament
+  def player_team(player)
+    Team.joins(:players).where( 'players_teams.player_id = ?', player.id ).uniq.last
   end
 end
