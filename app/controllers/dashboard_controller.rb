0e3ce9ee16bb8d53183b9b7eb556b62c0c58694e
Luis Mondesi <lemsx1@gmail.com>
2015-10-09 04:05:39 -0400

    allows users to sign up

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    if user_signed_in?
+    if user_signed_in? and current_user.player
       @teams = current_user.player.teams.paginate(page: params[:page], per_page: 15)
       @open_tournaments = current_user.player.open_tournaments.paginate(page: params[:otpage], per_page: 15)
       @past_tournaments = current_user.player.past_tournaments.paginate(page: params[:ptpage], per_page: 15)
