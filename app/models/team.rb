461752117ba0490a13982830fcb20a5e4f1649de
Luis Mondesi <lemsx1@gmail.com>
2015-10-09 02:34:43 -0400

    comments

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,9 @@ class Team < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
 
+  #TODO see below. should we try to validate that teams are already in this tournament?
+  #validate :only_one_division_per_tournament #, on: :update
+
   has_and_belongs_to_many :players
   # you cannot join a tournament, you always are in a division of a tournament
   has_and_belongs_to_many :divisions
@@ -18,4 +21,11 @@ class Team < ActiveRecord::Base
   #  logger.info attributes
   #  new_record? or true #FIXME
   #end
+
+  # TODO
+#   def only_one_division_per_tournament
+#     logger.info "checking if team is already in this tournament"
+#     team_divisions_for_tournament = divisions.where('tournament_id = ?',1)
+#     errors.add(:base, 'A team can only be in 1 division per tournament') unless team_divisions_for_tournament.size == 0
+#   end
 end
