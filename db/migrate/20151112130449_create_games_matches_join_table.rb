78fde32eba253f810ab4592f9412536470e212e9
Luis Mondesi <lemsx1@gmail.com>
2016-05-04 09:23:20 -0400

    do not need this oldie

--- db/migrate/20151112130449_create_games_matches_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateGamesMatchesJoinTable < ActiveRecord::Migration
-  def change
-    create_join_table :games, :matches do |t|
-      t.index [:game_id, :match_id]
-      t.index [:match_id, :game_id]
-    end
-  end
-end
