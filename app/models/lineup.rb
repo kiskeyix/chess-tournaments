f1cab7a42efc63c47995ac7f6f6f3fb443821b13
Luis Mondesi <lemsx1@gmail.com>
2016-05-16 19:09:58 -0400

    a lineup belongs to both a team and a match

--- app/models/lineup.rb
+++ app/models/lineup.rb
@@ -1,4 +1,7 @@
 class Lineup < ActiveRecord::Base
+  belongs_to :match
+  belongs_to :team
+
   has_many :lineups_line_items
   accepts_nested_attributes_for :lineups_line_items, allow_destroy: true
 end
